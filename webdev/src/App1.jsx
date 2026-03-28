0dfc0321c7088cfecfef79974c2c3e3d4c42429a
DaiwikCodeOtaku <rathordaiwik@gmail.com>
2026-03-29 01:28:57 +0530

    Create App1.jsx

---
+++ webdev/src/App1.jsx
@@ -0,0 +1,11 @@
+import CollectiblesLayout from "./components/project1"; // adjust path if needed
+
+function App1() {
+  return (
+    <>
+      <CollectiblesLayout />
+    </>
+  );
+}
+
+export default App1;
